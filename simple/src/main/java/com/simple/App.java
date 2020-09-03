5ce1372025f0cb39d5f77afc4676fadd14a8c442
fangkun <kun_ja@163.com>
2020-09-03 20:18:55 +0800

    1

--- simple/src/main/java/com/simple/App.java
+++ simple/src/main/java/com/simple/App.java
@@ -2,7 +2,6 @@ package com.simple;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
