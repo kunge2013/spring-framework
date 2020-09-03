38e753a006d7eb8c4a6fdabc73f6b55ea70307b4
fangkun <kun_ja@163.com>
2020-09-03 20:16:25 +0800

    1

--- simple/src/main/java/com/simple/App.java
+++ simple/src/main/java/com/simple/App.java
@@ -1,11 +1,14 @@
 package com.simple;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		User bean = context.getBean(User.class);
 		System.out.println(bean.toString());
 	}
+
 }
---
+++ simple/src/main/java/com/simple/AppConfig.java
@@ -0,0 +1,18 @@
+package com.simple;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.simple")
+public class AppConfig {
+
+	@Bean
+	public User createBean() {
+		User user = new User();
+		user.setAddress("wuhan");
+		user.setName("zhangsan");
+		return user;
+	}
+}
