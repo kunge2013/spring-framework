2760d2da0a06a06475b6dc9dcff9675806eef72b
Test Git <710784360@qq.com>
2020-09-09 17:54:40 +0800

    BidiMapTest LazyMapTest ListOrderedMapTest LRUMapTest MutliMapTest add

---
+++ simple/src/main/java/com/simple/comms-collections/BidiMapTest.java
@@ -0,0 +1,22 @@
+package com.cratos.game.flicker;
+
+import org.apache.commons.collections4.BidiMap;
+import org.apache.commons.collections4.bidimap.DualTreeBidiMap;
+
+/**
+ * @author fangkun
+ * @date 2020/9/9 17:08
+ * @description:
+ */
+public class BidiMapTest {
+    public static void main(String[] args) {
+        BidiMap<String, String> bidiMap = new DualTreeBidiMap();
+        {
+            bidiMap.put("A", "a");
+            bidiMap.put("B", "b");
+            bidiMap.put("C", "c");
+            System.out.println(bidiMap.get("A"));
+            System.out.println(bidiMap.getKey("a"));
+        }
+    }
+}
---
+++ simple/src/main/java/com/simple/comms-collections/MutliMapTest.java
@@ -0,0 +1,12 @@
+package com.cratos.game.flicker;
+
+/**
+ * @author fangkun
+ * @date 2020/9/9 17:06
+ * @description:
+ */
+public class MutliMapTest {
+    public static void main(String[] args) {
+        
+    }
+}
